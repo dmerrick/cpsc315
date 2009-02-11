e955cb5573c344e54e3705a969b64327e27d1c96
Dana Merrick <dana@ics.com>
2009-02-11 15:41:50 -0500

    Lots of updates for command parsing.

--- project2/ftpserver.c
+++ project2/ftpserver.c
@@ -18,14 +18,17 @@
 int main(void)
 {
 	char buf[BUFSIZ];
-	int server_sockfd, client_sockfd, client_len;
+	unsigned int server_sockfd, client_sockfd, client_len;
 	struct sockaddr_in client_address, server_address;
 	int len, i;
+  char cmd[4]; // this string will hold the command issued
+
 
 	if ((server_sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		perror("generate error");
 		exit(1);
 	}
+
 	memset(&server_address, 0, sizeof(server_address));
 	server_address.sin_family = AF_INET;
 	server_address.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -37,23 +40,54 @@ int main(void)
 		close(server_sockfd);
 		exit(2);
 	}
-	if (listen(server_sockfd, 5) < 0) {
+	
+  if (listen(server_sockfd, 5) < 0) {
 		perror("listen error");
 		exit(3);
 	}
-	client_len = sizeof(client_address);
-	if ((client_sockfd = accept(server_sockfd, 
+	
+  client_len = sizeof(client_address);
+	
+  if ((client_sockfd = accept(server_sockfd, 
 	    (struct sockaddr *) &client_address, &client_len)) < 0) {
 		perror("accept error");
 		close(server_sockfd);
 		exit(4);
 	}
+
 	while ((len=read(client_sockfd, buf, BUFSIZ)) > 0) {
-		for (i = 0; i < len; ++i)
+
+		for (i = 0; i < 4; ++i) {
+      // save the first for chars as a the command
+      // I used this instead of strncmp() so we could separate the arguements
+			cmd[i] = toupper(buf[i]);
+    }
+		for (i = 4; i < len; ++i) {
+      // save the rest as the command arguements
 			buf[i] = toupper(buf[i]);
+    }
+
+    // write the buffer back to the client
 		write(client_sockfd, buf, len);
+
+    printf("DEBUG: buf=%s",buf);
+    //printf("DEBUG: cmd=%s<<<\n",cmd);
+
+    if (strcmp(cmd,"PUT ") == 0) { 
+      puts("Command was PUT!"); 
+    }
+
+    if (strcmp(cmd,"GET ") == 0) { 
+      puts("Command was GET!"); 
+    }
+
+    // exit if period is sent
 		if (buf[0] == '.')
 			break;
 	}
+  // close the socket
 	close(client_sockfd);
+
+  // exit cleanly
+  return 0;
 }
