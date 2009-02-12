cccfa6033afe0b986e701d191e77e6737809d582
Dana Merrick <dana@ics.com>
2009-02-12 17:42:39 -0500

    More changes. Still little headway.

--- project2/ftpclient.c
+++ project2/ftpclient.c
@@ -14,6 +14,11 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int receiveFile(char file, unsigned int socket_fd);
+int sendFile(char file, unsigned int socket_fd);
 
 int main(int argc, char *argv[])
 {
@@ -21,13 +26,20 @@ int main(int argc, char *argv[])
 	int server_sockfd, len;
 	struct sockaddr_in server_address;
 	struct hostent *host;		/* the host (server) */
+  int i;
+  char cmd[4];
+	char file_arg[BUFSIZ];
 
+  // check arguements are sane
 	if (argc != 2) {
 		fprintf(stderr, "usage: %s server\n", argv[0]);
 		exit(1);
 	}
+
+  // get host by name
 	host = gethostbyname(argv[1]);
 	if (host == (struct hostent *) NULL) {
+    // or error
 		perror("gethostbyname ");
 		exit(2);
 	}
@@ -47,14 +59,116 @@ int main(int argc, char *argv[])
 		perror("connect error");
 		exit(4);
 	}
+
 	do {
 		write(STDOUT_FILENO, "> ", 3);
 		if ((len=read(STDIN_FILENO, buf, BUFSIZ)) > 0) {
 			write(server_sockfd, buf, len);
-			if ((len=read(server_sockfd, buf, BUFSIZ)) > 0) 
+			if ((len=read(server_sockfd, buf, BUFSIZ)) > 0) {
 				write(STDOUT_FILENO, buf, len);
-		}
-	} while (buf[0] != '.');
+
+     	  for (i = 0; i < 3; ++i) {
+          // save the first for chars as a the command
+          // I used this instead of strncmp() so we could separate the arguements
+			    cmd[i] = toupper(buf[i]);
+        }
+        cmd[3] = '\0';
+
+		    for (i = 4; i < len; ++i) {
+          // save the rest as the command arguement
+          file_arg[i-4]=buf[i];
+			    //buf[i] = toupper(buf[i]);
+        }
+
+        // write the buffer back to the client
+		    //write(client_sockfd, buf, len);
+
+        printf("DEBUG: file_arg=%s",file_arg);
+        printf("DEBUG: cmd=%s\n",cmd);
+        
+        if (strcmp(cmd,"PUT") == 0) { 
+
+          puts("Command was PUT!"); 
+          if (sendFile(file_arg, server_sockfd))
+            break;
+    
+        } else if (strcmp(cmd,"GET") == 0) { 
+          
+          puts("Command was GET!"); 
+          if (receiveFile(file_arg,server_sockfd))
+            break;
+    
+        } else if (strcmp(cmd,"BYE") == 0) { 
+		
+          puts("Command was BYE!"); 
+          exit(0);
+    
+        } else {
+    
+          printf("unrecognised command: %s",cmd);
+
+        }
+
+		  }
+    }
+	} while (strcmp(cmd,"BYE") != 0);
+
+  // close the socket
 	close(server_sockfd);
+  // and exit cleanly
 	exit(0);
 }
+
+
+int sendFile(char file, unsigned int socket_fd) {
+  char file_buf[BUFSIZ];
+  int file_len;
+  int local; // the local file
+
+  puts("Command was GET!"); 
+
+  local = open(file, O_RDONLY);
+
+  if (!local) {
+    fprintf(stderr, "ERROR: local file could not be opened: %s\n", file);
+  }
+
+	while ((file_len=read(socket_fd, file_buf, BUFSIZ)) > 0) {
+    // write to the socket
+    if (write(socket_fd,file_buf,file_len) < file_len) {
+      fprintf(stderr, "ERROR: error writing to socket");
+      return 2;
+    }
+  }
+
+  close(local);
+
+  return 0;
+}
+
+int receiveFile(char file, unsigned int socket_fd) {
+  char file_buf[BUFSIZ];
+  int file_len;
+  int local; // the local file
+
+  puts("Command was PUT!"); 
+  
+  local = open(file, O_WRONLY | O_CREAT | O_TRUNC, 00644);
+
+  
+  if (!local) {
+    fprintf(stderr, "ERROR: local file could not be opened: %s\n", file);
+  }
+
+  /*
+	if ((file_len=read(socket_fd, file_buf, BUFSIZ)) > 0) {
+    printf("%s\n",file_buf);
+	  write(local, file_buf, file_len);
+  }
+  */
+
+  close(local);
+
+  return 0;
+}
+
--- project2/ftpserver.c
+++ project2/ftpserver.c
@@ -17,16 +17,17 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+int receiveFile(char file, unsigned int socket_fd);
+int sendFile(char file, unsigned int socket_fd);
 
 int main(void)
 {
-	char buf[BUFSIZ], file_buf[BUFSIZ];
+	char buf[BUFSIZ];
 	unsigned int server_sockfd, client_sockfd, client_len;
 	struct sockaddr_in client_address, server_address;
-	int len, i, file_len;
+	int len, i;
   char cmd[4]; // this string will hold the command issued
   char file_arg[BUFSIZ]; // the arguement passed from the client
-  int local, remote; // the local and remote files, respectively
 
 
   // open socket or die trying
@@ -74,7 +75,7 @@ int main(void)
     cmd[3] = '\0';
 
 		for (i = 4; i < len; ++i) {
-      // save the rest as the command arguements
+      // save the rest as the command arguement
       file_arg[i-4]=buf[i];
 			//buf[i] = toupper(buf[i]);
     }
@@ -87,24 +88,13 @@ int main(void)
 
     if (strcmp(cmd,"PUT") == 0) { 
 
-      puts("Command was PUT!"); 
-      local = open(file_arg, O_WRONLY | O_CREAT | O_TRUNC, 00644);
-
-       if (!local) {
-         fprintf(stderr, "ERROR: local file could not be opened.", local);
-       }
-
-	    while ((file_len=read(client_sockfd, file_buf, BUFSIZ)) > 0) {
-        file_buf[5]='\0';
-        printf("%s\n",file_buf);
-		    write(local, file_buf, file_len);
-      }
-      break;
-      //remote = open(local, O_RDONLY);
+      if( receiveFile(file_arg, client_sockfd) )
+        break;
 
     } else if (strcmp(cmd,"GET") == 0) { 
       
-      puts("Command was GET!"); 
+      if(sendFile(file_arg, client_sockfd))
+        break;
     
     } else if (strcmp(cmd,"BYE") == 0) { 
 		
@@ -119,12 +109,51 @@ int main(void)
 
 	}
 
-
   // close the socket and files
 	close(client_sockfd);
-  close(local);
-  //close(remote);
 
   // exit cleanly
   return 0;
 }
+
+int sendFile(char file, unsigned int socket_fd) {
+  char file_buf[BUFSIZ];
+  int file_len;
+  int local; // the local file
+
+  puts("Command was GET!"); 
+
+  local = open(file, O_RDONLY);
+
+  if (!local) {
+    fprintf(stderr, "ERROR: local file could not be opened: %s\n", file);
+  }
+
+  close(local);
+
+  return 0;
+}
+
+int receiveFile(char file, unsigned int socket_fd) {
+  char file_buf[BUFSIZ];
+  int file_len;
+  int local; // the local file
+
+  puts("Command was PUT!"); 
+  
+  local = open(file, O_WRONLY | O_CREAT | O_TRUNC, 00644);
+
+  if (!local) {
+    fprintf(stderr, "ERROR: local file could not be opened: %s\n", file);
+  }
+
+	if ((file_len=read(socket_fd, file_buf, BUFSIZ)) > 0) {
+	  write(STDOUT_FILENO, file_buf, file_len);
+	  write(local, file_buf, file_len);
+  }
+
+  close(local);
+
+  return 0;
+}
+
