cd70800d1e4f17984bf8c1dfba5302eac10fe0ee
Dana Merrick <dana@ics.com>
2009-03-24 18:52:50 -0400

    Lots of cleanup all around.

--- project3/banker.c
+++ project3/banker.c
@@ -23,11 +23,12 @@ int init( char *filename );
 int resources, processes;
 
 // the global variables required for the Banker's Algorithm
+int *Available;
 int **Allocation;
 int **Max;
-int *Available;
-int *Available_tmp;
-int *maxres;
+int *Need;
+
+// the the list of all running processes
 int *Running;
 
 // loop variables to comply with C90 standard
@@ -37,20 +38,20 @@ int i,j;
  * Main method, reads from text file, initializes variables, and loops until finished.
  */
 int main(int argc, char *argv[]) {
-	
-  // check that we provided an arguement
-  if( argc < 2 ) {
-    printf("usage: %s config_file\n", argv[0]);
-    return 1;
-  }
 
-  // set up variables using config file
+	// check that we provided an arguement
+	if( argc < 2 ) {
+		printf("usage: %s config_file\n", argv[0]);
+		return 1;
+	}
+
+	// set up variables using config file
 	init(argv[1]); 
 
 	// initialize running to true for all processes 
-  for( i=0; i<processes; i++)
-    Running[i] = TRUE;
-	
+	for( i=0; i<processes; i++)
+		Running[i] = TRUE;
+
 	// start the main loop
 	int count = 0;
 	do { 
@@ -61,17 +62,18 @@ int main(int argc, char *argv[]) {
 				count++; 
 		} 
 
-    // show the current resourse status
-		printResources();
-		
+		// run the algorithm	
+		bankers();
+
 		// spawn threads and request resourses here.
 
-    // temporary loop kill
+		// temporary loop kill
 		count = 0;
 
 	} while( count != 0 );
 
 	// out of the while loop, so all of the processes are finished
+	printf("\n = Success! =\n");
 	printf("All processes finished without deadlock.\n"); 
 	return 0;
 }
@@ -84,81 +86,93 @@ int main(int argc, char *argv[]) {
  * @return true if safe, false if unsafe
  */
 int bankers() {
+	printf("\n    == Running Banker's Algorithm ==\n");
+
 	// assume the state is unsafe
-  int safe = FALSE;
+	int safe = FALSE;
 
-  // copy the Available array into a temporary array
-  for( i=0; i<resources; i++ ) {
-    Available_tmp[i] = Available[i];
-  }
+	// set up Available_tmp array
+	int *Available_tmp;
+	if( !(Available_tmp = malloc( resources * sizeof( int ) )))
+		return -1;
 
-  // look at each running process
-  for( i=0; i<processes; i++ ) {
-    if( Running[i] == TRUE ) {
-      if( ((Max[i][0] - Allocation[i][0]) <= Available_tmp[0]) /* && rest here */) {
-        for( j=0; j < resources; j++ ) {
-          Available_tmp[j] += Allocation[i][j];
-        }
-        safe = TRUE;
-      }
-    }
-  }
-  return safe;
+	// copy the Available array into the temporary array
+	for( i=0; i<resources; i++ ) {
+		Available_tmp[i] = Available[i];
+	}
+
+	// look at each running process
+	for( i=0; i<processes; i++ ) {
+		if( Running[i] == TRUE ) {
+			if( ((Max[i][0] - Allocation[i][0]) <= Available_tmp[0]) /* && rest here */) {
+				for( j=0; j < resources; j++ ) {
+					Available_tmp[j] += Allocation[i][j];
+				}
+				safe = TRUE;
+			}
+		}
+	}
+
+	printf("Result is: %s.\n", safe ? "SAFE" : "UNSAFE");
+	printResources();
+	return safe;
 }
 
 /**
  * Print current system status to the screen.
  */
 void printResources() {
-  printf("\n    == Current system resources ==");
+	printf("\n    == Current system resources ==");
 
-  // print status of maxres
-  printf("\n  === maxres ===\n");
-	for( i=0; i<resources; i++ ) 
-		printf("R%d\t", i+1);
-	printf("\n");
-	
-	for( i=0; i<resources; i++ ) 
-		printf("%d\t", maxres[i]); 
-		
 	// print status of Available
-	printf("\n\n  === Available ===\n"); 
+	printf("\n  === Available ===\n"); 
 	for( i=0; i<resources; i++ ) 
-		printf("R%d\t", i+1);
+		printf("\tR%d", i);
 	printf("\n");
-	
+
 	for( i=0; i<resources; i++ ) 
-		printf("%d\t", Available[i]);
-	
-	// print status of Max
-	printf("\n\n  === Max ===\n"); 
-	for( i=0; i<resources; i++ ) 
-		printf("\tR%d", i+1);
-	printf("\n");
-	
-	for( i=0; i<processes; i++) { 
-		if( Running[i] == TRUE ) { 
-			printf("P%d\t", i+1); 
-			for( j=0; j<resources; j++) 
-				printf("%d\t", Max[i][j]); 
-			printf("\n"); 
-		} 
-	}
-	
+		printf("\t%d", Available[i]);
+
 	// print status of Allocation
 	printf("\n\n  === Allocation ===\n"); 
 	for( i=0; i<resources; i++ ) 
-		printf("\tR%d",i+1);
+		printf("\tR%d",i);
 	printf("\n");
-	
+
 	for( i=0; i<processes; i++) { 
 		if( Running[i] == TRUE ) {
-			printf("P%d\t", i+1); 
+			printf("P%d\t", i); 
 			for( j=0; j<resources; j++) 
 				printf("%d\t", Allocation[i][j]); 
 			printf("\n"); 
 		} 
 	}
+
+	// print status of Max
+	printf("\n  === Max ===\n"); 
+	for( i=0; i<resources; i++ ) 
+		printf("\tR%d", i);
+	printf("\n");
+
+	for( i=0; i<processes; i++) { 
+		if( Running[i] == TRUE ) { 
+			printf("P%d\t", i); 
+			for( j=0; j<resources; j++) 
+				printf("%d\t", Max[i][j]); 
+			printf("\n"); 
+		} 
+	}
+
+	// print status of Need
+	printf("\n  === Need ===\n");
+	for( i=0; i<resources; i++ ) 
+		printf("\tR%d", i);
+	printf("\n");
+
+	for( i=0; i<resources; i++ ) 
+		printf("\t%d", Need[i]); 
+	printf("\n"); 
+
 }
 
 /**
@@ -169,22 +183,22 @@ void printResources() {
  */
 int init( char *filename ) {
 
-  // open the config file
+	// open the config file
 	FILE *file;
 	if( !(file = fopen(filename,"r"))) {
 		printf("error: can't open file %s\n",filename);
 		return 9;
 	}
-	
+
 	char buffer[MAX]; // the line read from the file
 	char *tmp;        // each value on a line as a string
-	
+
 	// parse the first line: number of resource types
 	fgets(buffer, MAX, file);
 	resources = atoi(buffer);
-	
-  // now we can set up the maxres array
-	if( !(maxres = malloc( resources * sizeof( int ) )))
+
+	// now we can set up the Need array
+	if( !(Need = malloc( resources * sizeof( int ) )))
 		return 6;
 
 	// parse second line: number of instances of each resource type
@@ -193,17 +207,17 @@ int init( char *filename ) {
 	// we'll use a tokenizer to split the string on spaces
 	tmp = strtok(buffer," ");
 	while( tmp != NULL) {
-		maxres[i] = atoi(tmp);
+		Need[i] = atoi(tmp);
 		tmp = strtok(NULL, " ");
 		i++;
 	}
-		  
+
 	// parse third line: number of processes
 	fgets(buffer, MAX, file);
 	processes = atoi(buffer);
-	
+
 	// now that we have both processes and resources
-  // we can set up the Max array (2D)
+	// we can set up the Max array (2D)
 	if( !(Max = malloc( processes * sizeof( int* ) )))
 		return 3;
 	for( i=0; i<processes; i++ )
@@ -224,9 +238,9 @@ int init( char *filename ) {
 		i++;
 	}
 
-  // now we can finish creating the other arrays
+	// now we can finish creating the other arrays
 
-  // set up Allocation array (2D)
+	// set up Allocation array (2D)
 	if( !(Allocation = malloc( processes * sizeof( int* ) )))
 		return 1;
 	for( i=0; i<processes; i++ )
@@ -240,10 +254,6 @@ int init( char *filename ) {
 	// set up Available array
 	if( !(Available = malloc( resources * sizeof( int ) )))
 		return 7;
-
-	// set up Available_tmp array
-	if( !(Available_tmp = malloc( resources * sizeof( int ) )))
-		return 8;
 
 	// close the file and exit cleanly
 	fclose(file);
