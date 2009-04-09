5d91f4f9c6077870ed309f5594d63718aad52df8
Dana Merrick <dana@ics.com>
2009-04-09 13:58:10 -0400

    Cleanup. Strange error with some directorys...

--- project4/tree.c
+++ project4/tree.c
@@ -6,22 +6,38 @@
 #include <errno.h>
 #include <string.h>
 
+void parse_dir(char*, int);
 
 /**
  * Prints a graphical view of the directory structure.
  */
 int main(int argc, char *argv[]) {
 
-  char *start_location = ".";
+  // root of the tree
+  char *start_location;
+
+  // check if we provided an arguement
+  if( argc < 2 ) {
+    // use the current dir
+    start_location = ".";
+  } else {
+    // use the provided dir
+    start_location = argv[1];
+  }
 
   // run on start location
   parse_dir(start_location,0);
 
-
+  // exit cleanly
   return 0;
 
 }
 
+/**
+ * Recursively prints an indented directory structure of the current dir.
+ * @param name of directory to parse
+ * @param level of indentation to use
+ */
 void parse_dir( char *dir_name, int indent ) {
   DIR *dir_p;
 
@@ -29,7 +45,7 @@ void parse_dir( char *dir_name, int indent ) {
 
   // check that we opened correctly
   if ( dir_p == NULL ) {
-      printf(stderr, "opendir() failed\n");
+      fprintf(stderr, "opendir() failed\n");
       exit(-1);
   }
 
@@ -38,6 +54,7 @@ void parse_dir( char *dir_name, int indent ) {
   int i;
 
   dirent_p = readdir( dir_p );
+
   while ( dirent_p != NULL ) {
     name = dirent_p->d_name;
 
@@ -47,11 +64,13 @@ void parse_dir( char *dir_name, int indent ) {
       for( i=0; i<indent; i++)
         printf("  ");
 
+      // print the name
       printf( "%s\n", name );
 
-      if (dirent_p->d_type == DT_DIR) {
+      // recurse if we found a directory
+      if (dirent_p->d_type == DT_DIR)
         parse_dir(name,indent+1);
-      } 
+
     }
 
     // move to the next file in the dir
