fa4e91047c2d6c441a74f83e1d920f8acd0ac901
Dana Merrick <dana@ics.com>
2009-04-10 07:49:01 -0400

    Finished question 3. Still needs mode help.

--- project4/myls.c
+++ project4/myls.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <dirent.h>
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <time.h>
+#include <pwd.h>
+#include <grp.h>
 
 // method signature
 void print_file( struct dirent* );
+int file_select( struct dirent* );
 
 /**
  * Prints a graphical view of the directory structure.
@@ -17,9 +22,10 @@ int main(int argc, char *argv[]) {
   // directory to list
   char *dir_name;
 
-  DIR *dir_p;
-  struct dirent *dirent_p;
-  char *name;
+  // directory listing
+  struct dirent **dir_list;
+
+  int count, i;
 
 
   // check if we provided an arguement
@@ -31,36 +37,54 @@ int main(int argc, char *argv[]) {
     dir_name = argv[1];
   }
 
+  // read directory listing into array
+  count = scandir(dir_name, &dir_list, file_select, alphasort);
 
-  dir_p = opendir(dir_name); 
-
-  // check that we opened correctly
-  if ( dir_p == NULL ) {
-      fprintf(stderr, "opendir() failed\n");
-      exit(-1);
-  }
-
-  dirent_p = readdir( dir_p );
-
-  while ( dirent_p != NULL ) {
-    name = dirent_p->d_name;
-
-    // print the name
-    print_file(dirent_p);
-
-    // move to the next file in the dir
-    dirent_p = readdir( dir_p );
-  }
-
-  closedir(dir_p);
+  // loop through each file
+  for (i=1; i<count+1; ++i)
+    print_file(dir_list[i-1]);
 
   // exit cleanly
   return 0;
 
 }
 
+int file_select(struct dirent *dirent_p) {
+   
+  if (strcmp(dirent_p->d_name,".")==0 || strcmp(dirent_p->d_name, "..")==0)
+    return 0;
+  else
+    return 1;
+}
+
 void print_file( struct dirent *dirent_p) {
   char *name = dirent_p->d_name;
-  printf( "%s\n", name );
+
+  // get file stats
+  struct stat buffer;
+  lstat(name, &buffer);
+
+  // user and group information
+  struct passwd *pwd = getpwuid(buffer.st_uid);
+  struct group *grp = getgrgid(buffer.st_gid);
+  char *gid = grp->gr_name;
+  char *pwid = pwd->pw_name;
+
+  // last modified time
+  time_t lastmod = buffer.st_mtime;
+  char time[80];
+
+  // mode
+  unsigned long mode = buffer.st_mode;
+
+  // links and size
+  long int links = buffer.st_nlink;
+  long int size = buffer.st_size;
+
+  // format time
+  strftime(time, sizeof(time), "%Y-%m-%d %H:%M", localtime(&lastmod));
+
+  printf("%lo %ld %s %s %ld %s %s\n", mode, links, pwid, gid, size, time, name);
+
 }
 
